6926a432642b3ce9f7bc2354fc327cb5d37e8087
nayasky94 <37323952+nayasky94@users.noreply.github.com>
2026-03-04 13:37:12 +0900

    feat: GET /api/sites 구현 (Site Entity, Repository, Service)

--- src/main/java/com/jj/redline/config/RateLimitConfig.java
+++ src/main/java/com/jj/redline/config/RateLimitConfig.java
@@ -10,8 +10,10 @@ import io.lettuce.core.codec.StringCodec;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
+@Profile("prod")
 @Configuration
 public class RateLimitConfig {
 
