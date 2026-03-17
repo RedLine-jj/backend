c9073a0b5945cacdb5eb8a2166fade547f270dc8
nayasky94 <37323952+nayasky94@users.noreply.github.com>
2026-03-17 11:47:02 +0900

    feat: Redis 캐시 인프라 설정 (TTL별 캐시 매니저)

---
+++ src/main/java/com/jj/redline/config/RedisCacheConfig.java
@@ -0,0 +1,38 @@
+package com.jj.redline.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .disableCachingNullValues();
+
+        Map<String, RedisCacheConfiguration> cacheConfigs = Map.of(
+                "sites", defaultConfig.entryTtl(Duration.ofHours(24)),
+                "brands", defaultConfig.entryTtl(Duration.ofHours(24)),
+                "subscriptions:top", defaultConfig.entryTtl(Duration.ofHours(24)),
+                "restocks:recent", defaultConfig.entryTtl(Duration.ofMinutes(5))
+        );
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfig.entryTtl(Duration.ofMinutes(10)))
+                .withInitialCacheConfigurations(cacheConfigs)
+                .build();
+    }
+}
